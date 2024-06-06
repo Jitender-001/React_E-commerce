f36ae444dab70fa56d86cb263d3036e99486f7be
Jitender Yadav <121676248+Jitender-001@users.noreply.github.com>
2024-06-06 13:08:36 +0530

    Add files via upload

---
+++ home_products.js
@@ -0,0 +1,68 @@
+const HomeProducts = [
+    {
+        id:1,
+        Name : 'Product1',
+        price : '26',
+        image : 'images/product1.jpg',
+        cat :  '',
+        type : 'new',
+    },
+    {
+        id:2,
+        Name : 'Product2',
+        price : '70',
+        image : 'images/product2.jpg',
+        cat :  '',
+        type : 'featured',
+    },
+    {
+        id:3,
+        Name : 'Product3',
+        price : '92',
+        image : 'images/product3.jpg',
+        cat :  '',
+        type : 'new',
+    },
+    {
+        id:4,
+        Name : 'Product4',
+        price : '110',
+        image : 'images/product4.jpg',
+        cat :  '',
+        type : 'featured',
+    },
+    {
+        id:5,
+        Name : 'Product5',
+        price : '96',
+        image : 'images/product5.jpg',
+        cat :  '',
+        type : 'top',
+    },
+    {
+        id:6,
+        Name : 'Product6',
+        price : '80',
+        image : 'images/product6.jpg',
+        cat :  '',
+        type : 'New',
+    },
+    {
+        id:7,
+        Name : 'Product7',
+        price : '76',
+        image : 'images/product7.jpg',
+        cat :  '',
+        type : 'top',
+    },
+    {
+        id:8,
+        Name : 'Product8',
+        price : '105',
+        image : 'images/product8.jpg',
+        cat :  '',
+        type : 'top',
+    }
+]
+
+export default HomeProducts;
