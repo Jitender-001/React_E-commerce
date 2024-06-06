3ab17285b21996a41dfc7ecfaa4984d0c75fd563
Jitender Yadav <121676248+Jitender-001@users.noreply.github.com>
2024-06-06 13:09:57 +0530

    Update home.js

--- home.js
+++ home.js
@@ -1,8 +1,29 @@
 // import style from './home.module.css';
 import './home.css';
 import { Link } from 'react-router-dom';
+import HomeProducts from './home_products';
+import { useState } from 'react';
+import { FaEye } from "react-icons/fa";
+import { FaHeart } from "react-icons/fa";
 
 function Home() {
+
+    const [trendingProduct,setTrendingProduct] = useState(HomeProducts);
+
+    //filtering trending products
+
+    const filterCategory = (x)=>{
+        const filterProduct = HomeProducts.filter((currEle)=>{
+            return currEle.type === x;
+        })
+        setTrendingProduct(filterProduct);
+    }
+
+    //displaying all the trending items
+    const allTrendings = ()=>{
+        setTrendingProduct(HomeProducts);
+    }
+
     return (
         <>
             <div className="home">
@@ -20,16 +41,53 @@ function Home() {
                 </div>
 
                 <div className="trending">
-                    <div className="container">
+                    <div className="container1">
                         <div className="left-box">
                             <div className="header">
                                 <div className="heading">
-                                    <h2>Trending Items</h2>
+                                    <h2 onClick={()=>allTrendings()}>Trending Items</h2>
                                 </div>
                                 <div className="categories">
-                                    <h3>New</h3>
-                                    <h3>Featured</h3>
-                                    <h3>Top Selling</h3>
+                                    <h3 onClick={()=> filterCategory('new')}>New</h3>
+                                    <h3 onClick={()=> filterCategory('featured')}>Featured</h3>
+                                    <h3 onClick={()=> filterCategory('top')}>Top Selling</h3>
+                                </div>
+                            </div>
+
+                            <div className="products">
+                                <div className="container1">
+                                    {
+                                        trendingProduct.map((currEle)=>{
+                                            return(
+                                                <>
+                                                    <div className="box">
+                                                        <div className="img-box">
+                                                            <img src={currEle.image} alt="Product-image" />
+
+                                                            <div className="icon">
+
+                                                                <div className="icon-box">
+                                                                    <FaEye />
+                                                                </div>
+
+                                                                <div className="icon-box">
+                                                                    <FaHeart />
+                                                                </div>
+
+                                                            </div>
+
+                                                        </div>
+
+                                                        <div className="info">
+                                                            <h3>{currEle.Name}</h3>
+                                                            <p>${currEle.price}</p>
+                                                            <button className='btn'>Add To Cart</button>
+                                                        </div>
+                                                    </div>
+                                                </>
+                                            )
+                                        })
+                                    }
                                 </div>
                             </div>
                         </div>
